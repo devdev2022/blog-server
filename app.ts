91c81210173656cd3b0d346d721266416875cf50
khs <devdev0226@gmail.com>
2026-02-25 18:01:32 +0900

    개발 환경세팅

---
+++ app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+// import { router } from "./src/forrest/routes";
+// import { globalErrorHandler } from "./src/forrest/utils/error";
+
+const createApp = () => {
+  const app = express();
+
+  app.use(express.static("src/utils/swagger"));
+
+  app.use(cookieParser());
+
+  //   app.use(
+  //     cors({
+  //       origin: ["http://127.0.0.1:3001", "http://localhost:3001"],
+  //       credentials: true,
+  //       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  //       allowedHeaders: ["Content-Type", "Authorization"],
+  //     }),
+  //   );
+  //   app.use(express.json());
+  //   app.use(morgan("combined"));
+
+  //   app.use(router);
+  //   app.use(globalErrorHandler);
+
+  return app;
+};
+
+export { createApp };
---
+++ data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ["dist/entity/forrest/*.js"],
+  synchronize: true,
+});
