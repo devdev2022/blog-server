0cd46e7538e33043fd0c7135cde7596018364aba
khs <devdev0226@gmail.com>
2026-03-24 20:29:55 +0900

    Fix : SSE 연결 시간 설정 수정

--- src/notifications/sseManager.ts
+++ src/notifications/sseManager.ts
@@ -2,6 +2,13 @@ import { Response } from "express";
 
 class SseManager {
   private clients = new Set<Response>();
+  private heartbeatInterval: ReturnType<typeof setInterval>;
+
+  constructor() {
+    this.heartbeatInterval = setInterval(() => {
+      this.clients.forEach((client) => client.write(": ping\n\n"));
+    }, 30000);
+  }
 
   addClient(res: Response) {
     this.clients.add(res);
